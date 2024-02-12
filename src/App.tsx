63b85a294d3ed65cb27e5a1cfd8d7cb875434e44
hdao35 <hdao35@gatech.edu>
2024-02-11 21:26:05 -0500

    add State class and custom marker icon for google maps

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import config from "./config.json";
 
-import { GoogleMap, useLoadScript, Marker, Libraries } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, MarkerF, Libraries } from '@react-google-maps/api';
 
 const API_KEY = config["API_KEY"];
 
 const libraries : Libraries = ['places'];
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: '80vw',
+  height: '80vh',
 };
 const center = {
-  lat: 7.2905715, // default latitude
-  lng: 80.6337262, // default longitude
+  lat: 39.5, // default latitude
+  lng: -96, // default longitude
 };
+
+const defaultIconSize = 15;
+
+class State {
+  name: string;
+  lat: number;
+  lng: number;
+
+  constructor(name: string, lat: number, lng: number) {
+    this.name = name;
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
+
+const states: State[] = [
+  new State("Washington", 47.49330662463946, -120.22133558330367),
+  new State("Oregon", 43.93572461347163, -120.79262465538007)
+]
 
 const App = () => {
   const { isLoaded, loadError } = useLoadScript({
@@ -35,10 +54,12 @@ const App = () => {
     <div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={10}
+        zoom={4.5}
         center={center}
       >
-        <Marker position={center} />
+        {states.map((state, index) => (
+          <MarkerF position={{lat: state.lat, lng: state.lng}} icon={{url:require("./images/cat.png"), scaledSize: new google.maps.Size(defaultIconSize,defaultIconSize)}}/>
+        ))}
       </GoogleMap>
     </div>
   );
